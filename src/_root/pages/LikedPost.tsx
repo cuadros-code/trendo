167e5a983a0ab7aecb37f9e2601089178e44b673
Kevin Cuadros <kevindavidcuadros@gmail.com>
2024-02-15 21:25:43 -0500

    Add LikedPost component and update Bottombar

---
+++ src/_root/pages/LikedPost.tsx
@@ -0,0 +1,9 @@
+const LikedPost = () => {
+  return (
+    <div>
+      <h1>Liked Post</h1>
+    </div>
+  );
+}
+ 
+export default LikedPost;
--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,8 +1,36 @@
+import { bottombarLinks } from '@/constants';
+import { Link, useLocation } from 'react-router-dom'
+
 const Bottombar = () => {
+
+  const { pathname } = useLocation()
+
   return (
-    <div>
-      
-    </div>
+    <section className="bottom-bar">
+      {
+        bottombarLinks.map((link, index) => {
+          
+          const isActive = pathname === link.route
+
+          return (
+            <Link
+              to={link.route}
+              key={index}
+              className={`${isActive && 'bg-primary-500 rounded-[10px]'} flex-center flex-col gap-1 p-2 transition`}
+            >
+              <img 
+                src={link.imgURL} 
+                alt={link.label}
+                width={18}
+                height={18}
+                className={`${isActive && 'invert-white' }`}
+              />
+              <p className='tiny-medium text-light-2' >{link.label}</p>
+            </Link>
+          )
+        })
+      }
+    </section>
   );
 }
  
